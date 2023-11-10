0615f0e617bd63ae28927906a982c12d4183a17b
Macbook <macbook@michael.local>
2023-11-10 09:30:20 +0700

    view login & registration

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,9 +8,18 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
+
+     <!-- Bootstrap Icons -->
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+
     <title>Michael Blog | {{ $title }}</title>
   </head>
   <body>
+
+    <!-- My Styles -->
+    <link rel="stylesheet" href="/css/style.css">
+
+
 
     @include('partials.navbar')
 
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,15 @@
 <?php
 
 
-use App\Http\Controllers\PostController;
 use App\Models\Post;
-use Illuminate\Support\Facades\Route;
-use PostController as GlobalPostController;
-
 use App\Models\Category;
-use App\Models\User;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -58,6 +60,12 @@ Route::get('/categories', function() {
         'categories' => Category::all()
     ]);
 });
+
+
+Route::get('/login', [LoginController::class, 'index']);
+
+Route::get('/register', [RegisterController::class, 'index']);
+Route::post('/register', [RegisterController::class, 'store']);
 
 
 // Route::get('/categories/{category:slug}', function(Category $category) {
