2b4da9a00ac87aef52c57c2ccb174656fdf43e16
Macbook <macbook@michael.local>
2023-11-10 10:49:49 +0700

    User Registration 15

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
 
 class RegisterController extends Controller
 {
@@ -16,6 +19,20 @@ class RegisterController extends Controller
 
     public function store(Request $request)
     {
-        return $request->all();
+     $validatedData = $request->validate([
+        'name' => 'required|max:255',
+        'username' => ['required','min:3','max:255', 'unique:users'],
+        'email'=> 'required|email|unique:users',
+        'password' => 'required|min:5|max:255'
+        ]);
+
+        // $validatedData['password'] = bcrypt($validatedData['password']);
+        $validatedData['password'] = Hash::make($validatedData['password']);
+
+        User::create($validatedData);
+        
+        //$request->session()->flash('success', 'Registration succesfull! Please login');
+
+        return redirect('/login')->with('success','Registration successfull! Please login');
     }
 }
