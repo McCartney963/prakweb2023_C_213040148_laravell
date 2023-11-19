af41112869fb2a55c02f5f6a43f51fe53e6297d7
Macbook <macbook@michael.local>
2023-11-19 13:06:33 +0700

    Create Post Form 18

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, Sluggable;
+    
 
     protected $guarded = ['id'];
     protected $with = ['category', 'author'];
@@ -47,4 +49,14 @@ class Post extends Model
     {
         return 'slug';
     }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
 }
+
